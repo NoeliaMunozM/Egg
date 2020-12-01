334586b91cc34f2a7e4e5ebf07868d97a6f7b628
Noelia Muñoz <nsmm17@gmail.com>
2020-12-01 12:28:56 -0300

    Segundo Commit

--- src/estancias/servicios/CasaDAO.java
+++ src/estancias/servicios/CasaDAO.java
@@ -1,6 +1,35 @@
 
 package estancias.servicios;
 
+import estancias.entidades.Casas;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CasaDAO extends Conexion {
-    
+    public List<Casas> listarCasas() throws Exception {
+        try {
+            //Armo el sql
+            String sql = "SELECT id_casa, calle, numero"
+                    + "  FROM casas ";
+       
+            //Consulto la base de datos
+            consultarBase(sql);
+            //Recorro el resultados de la consulta
+            Casas casa = null;
+            List<Casas> casas = new ArrayList();
+            while (resultado.next()) {
+                casa = new Casas();
+                casa.setId(resultado.getInt(1));
+                casa.setCalle(resultado.getString(2));
+                casa.setNumero(resultado.getInt(3));
+                casas.add(casa);
+            }
+            return casas;
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new Exception("Error de sistema");
+        } finally {
+            desconectarBase();
+        }
+    }
 }
